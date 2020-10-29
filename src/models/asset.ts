e7137e4d29e6bfc0c6d21efb44fe111633e30d65
stemy <balazs.stemler@metrix.co.hu>
2020-10-29 18:00:13 +0100

    Fix asset transformations
    v1.2.1

--- src/models/asset.ts
+++ src/models/asset.ts
@@ -21,41 +21,48 @@ const AssetSchema = createSchema(
 );
 
 AssetSchema.methods.getImage = async function (params: IAssetImageParams = null) {
-    const asset = this as AssetDoc;
     params = params || {};
-    const paramKeys = Object.keys(params);
-    if (paramKeys.length == 0) return asset.stream;
+
+    const asset = this as AssetDoc;
+    if (Object.keys(params).length == 0) return asset.stream;
     let buffer = await streamToBuffer(asset.stream);
-    const rotation = Math.round((params.rotation || 0) / 90) * 90;
+
+    // Parse params
+    params.rotation = isNaN(params.rotation) ? 0 : Math.round(params.rotation / 90) * 90;
+    params.canvasScaleX = isNaN(params.canvasScaleX) ? 1 : params.canvasScaleX;
+    params.canvasScaleY = isNaN(params.canvasScaleY) ? 1 : params.canvasScaleY;
+    params.scaleX = isNaN(params.scaleX) ? 1 : params.scaleX;
+    params.scaleY = isNaN(params.scaleY) ? 1 : params.scaleY;
+
+    // Try to modify image
     try {
-        let sharpImg = sharp(buffer);
-        const meta = await sharpImg.metadata();
+        // Get metadata
+        const meta = await sharp(buffer).metadata();
         let width = meta.width;
         let height = meta.height;
         // Resize canvas
-        if (!isNaN(params.canvasScaleX) || !isNaN(params.canvasScaleY)) {
-            params.canvasScaleX = isNaN(params.canvasScaleX) ? 1 : params.canvasScaleX;
-            params.canvasScaleY = isNaN(params.canvasScaleY) ? 1 : params.canvasScaleY;
-            width *= params.canvasScaleX;
-            height *= params.canvasScaleY;
-            sharpImg.resize({width, height, background: "#00000000", fit: "contain"});
+        if (params.canvasScaleX !== 1 || params.canvasScaleY !== 1) {
+            width = Math.round(width * params.canvasScaleX);
+            height = Math.round(height * params.canvasScaleY);
+            buffer = await sharp(buffer)
+                .resize({width, height, background: "#00000000", fit: "contain"})
+                .toBuffer();
         }
         // Resize image
-        if (!isNaN(params.scaleX) || !isNaN(params.scaleY)) {
-            params.scaleX = isNaN(params.scaleX) ? 1 : params.scaleX;
-            params.scaleY = isNaN(params.scaleY) ? 1 : params.scaleY;
-            width *= params.scaleX;
-            height *= params.scaleY;
-            sharpImg.resize({width, height, background: "#00000000", fit: "fill"});
+        if (params.scaleX !== 1 || params.scaleY !== 1) {
+            width = Math.round(width * params.scaleX);
+            height = Math.round(height * params.scaleY);
+            buffer = await sharp(buffer)
+                .resize({width, height, background: "#00000000", fit: "fill"})
+                .toBuffer();
         }
         // Rotate
-        if (!isNaN(rotation) && rotation !== 0) {
-            sharpImg = sharpImg.rotate(rotation);
+        if (params.rotation !== 0) {
+            buffer = await sharp(buffer).rotate(params.rotation).toBuffer();
         }
-        buffer = await sharpImg.toBuffer();
         return bufferToStream(buffer);
     } catch (e) {
-        return asset.stream;
+        return bufferToStream(buffer);
     }
 }
 
